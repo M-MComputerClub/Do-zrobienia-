2c28e11911cf1c908bd4598bcb0ce21c861e8c3e
itsmaciek <maciej.matysiak@outlook.com>
2024-08-16 17:50:24 +0200

    fixed

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -155,7 +155,7 @@ onMounted(() => {
 
 	<footer class="flex justify-around fixed bottom-5 left-0 right-0 items-center">
       <div class="size-10 bg-zinc-500 items-center flex justify-center rounded-full">h</div>
-      <button class="size-12 bg-zinc-500 rounded-full text-2xl" @click="">+</button>
+      <button class="size-12 bg-zinc-500 rounded-full text-2xl" @click="add_menu = !add_menu">+</button>
       <div class="size-10 bg-zinc-500 items-center flex justify-center rounded-full">j</div>
     </footer>
 	</main>
